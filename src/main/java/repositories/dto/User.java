e7025aea740981caf24f105e77dc709a09aae547
Letícia Mangrich <leticiamangrichh@gmail.com>
2024-05-19 20:47:49 -0300

    Adicionado repo e DTO, mudanças no xml

---
+++ src/main/java/repositories/dto/User.java
@@ -0,0 +1,26 @@
+package repositories.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+public class User {
+    @JsonProperty("user_pk")
+    private Integer userPk;
+
+    @JsonProperty("picture_id")
+    private Integer pictureId;
+
+    @JsonProperty("name")
+    private String userName;
+
+    @JsonProperty("email")
+    private String userEmail;
+
+    @JsonProperty("password")
+    private String userPassword;
+}
