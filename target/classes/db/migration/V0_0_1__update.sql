177c65ab57d6d0e06737dc8fb2c935c46b8a0dd3
LMangrich <leticiamangrichh@gmail.com>
2024-05-20 13:10:50 -0300

    Adicionado .gitignore

--- target/classes/db/migration/V0_0_1__update.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `user` (
-    `user_pk` INT PRIMARY KEY AUTO_INCREMENT,
-    `picture_id` INT(11) NOT NULL,
-    `name` VARCHAR(40) NOT NULL,
-    `email` VARCHAR(40) NOT NULL,
-    `password` VARCHAR(40) NOT NULL
-);
